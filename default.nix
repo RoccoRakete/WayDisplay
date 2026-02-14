3530beab2a693ea84709bd3594f46305d825fdb0
martin <m.schaumann@mscloud.uk>
2026-02-14 16:31:47 +0100

    add DesktopFile

--- default.nix
+++ default.nix
@@ -1,46 +1,66 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+{ pkgs }:
+
+let
+  # Definition of the desktop entry
+  desktopItem = pkgs.makeDesktopItem {
+    name = "way-display";
+    exec = "way-display";
+    icon = "way-display";
+    comment = "A Wayland display utility built with egui";
+    desktopName = "WayDisplay";
+    genericName = "Display Utility";
+    categories = [ "Utility" ];
+    terminal = false;
+  };
+in
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "workflow";
-  version = "0.1";
+  pname = "way-display";
+  version = "0.1.0";
+
+  src = ./.;
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
 
   nativeBuildInputs = with pkgs; [
+    pkg-config
     rustc
     cargo
-    pkg-config
-    glib
-    glibc
-    lld
-    openssl
-    sqlite
-    gobject-introspection
-    dbus-glib
-    gtk4
-    gtk3
     makeWrapper
+    copyDesktopItems
   ];
 
   buildInputs = with pkgs; [
-    rustc
-    cargo
-    pkg-config
+    libxkbcommon
+    libGL
+    wayland
+    libX11
+    libXcursor
+    libXi
+    libXrandr
+
     glib
-    glibc
-    lld
     openssl
     sqlite
-    gobject-introspection
-    dbus-glib
-    gtk4
-    gtk3
+    dbus
   ];
 
+  desktopItems = [ desktopItem ];
+
   postInstall = ''
-    wrapProgram $out/bin/workflow \
+    wrapProgram $out/bin/way-display \
+      --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath buildInputs}"
+
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+
+    cp assets/monitor.jpg $out/share/icons/hicolor/256x256/apps/way-display.png
   '';
 
-  cargoLock.lockFile = ./Cargo.lock;
-
-  src = pkgs.lib.cleanSource ./.;
+  meta = with pkgs.lib; {
+    description = "A Wayland display utility built with egui";
+    homepage = "https://github.com/RoccoRakete/WayDisplay";
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
 }
